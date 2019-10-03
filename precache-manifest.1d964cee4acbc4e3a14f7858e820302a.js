e3e776b5e5803f552b1968be40a8399e93ce9aec
AlejandraMoralesB <a.moralesbarra@gmail.com>
2019-10-03 15:56:53 -0300

    Updates

---
+++ precache-manifest.1d964cee4acbc4e3a14f7858e820302a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d4b7df723a3571c0b63451f35c5f79a",
+    "url": "/AlejandraMoralesB/SCL010-Burger-Queen/index.html"
+  },
+  {
+    "revision": "6f71d21eeb01ddb74693",
+    "url": "/AlejandraMoralesB/SCL010-Burger-Queen/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "40b8e9cb1d589dc1748d",
+    "url": "/AlejandraMoralesB/SCL010-Burger-Queen/static/css/main.5e140aaa.chunk.css"
+  },
+  {
+    "revision": "6f71d21eeb01ddb74693",
+    "url": "/AlejandraMoralesB/SCL010-Burger-Queen/static/js/2.a4a46d49.chunk.js"
+  },
+  {
+    "revision": "40b8e9cb1d589dc1748d",
+    "url": "/AlejandraMoralesB/SCL010-Burger-Queen/static/js/main.9bd2019f.chunk.js"
+  },
+  {
+    "revision": "0c8d3b3eafcae7602a3f",
+    "url": "/AlejandraMoralesB/SCL010-Burger-Queen/static/js/runtime~main.65771cdf.js"
+  }
+]);
